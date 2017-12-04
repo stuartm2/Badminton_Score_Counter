bf699c575607e4e9a55446cadafbe80887b48a94
Stuart Metcalfe <stuartm2@gmail.com>
2017-12-04 17:57:12 +0000

    Send radio to sleep too

--- Handset/Handset.ino
+++ Handset/Handset.ino
@@ -48,7 +48,8 @@ void loop() {
   
   // Enter power down state with ADC and BOD module disabled.
   // Wake up when wake up pin is low.
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+  radio.sleep();
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
   
   // Disable external pin interrupt on wake up pin.
   detachInterrupt(1);
